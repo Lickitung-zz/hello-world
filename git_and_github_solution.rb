5d0095442cf644d1c665455aaf1437f0ba149691
Brandon Evans <bnevans@fb.com>
2019-02-19 14:50:10 -0600

    Add git_and_github_solution.rb file

---
+++ git_and_github_solution.rb
@@ -0,0 +1,7 @@
+# Paste a link to your github repository below:
+
+https://github.com/SIeep/hello-world
+
+# What questions do you have about what we've learned so far using git and github?:
+
+I think I asked Josh all of my questions. I just need to get comfortable with Git and Github workflow. I suppose branches are a bit confusing to me still.
